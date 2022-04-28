f1fdfbe879826d915b45dbfff0c091daf1e7ac21
franara <franara2001@gmail.com>
2022-04-28 19:48:39 -0300

    Commit con el desafío de eventos

--- scripts/desafio-clase4.js
+++ scripts/desafio-clase4.js
@@ -5,27 +5,42 @@ let nota;
 let notaAcum = 0;
 let alumnos = parseInt(prompt ("Ingrese la cantidad de alumnos."));
 
-
-for (i = 1 ; i <= alumnos ; i++) {
-    nota = parseInt(prompt("Ingrese la nota final del alumno"));
-    notaAcum = notaAcum + nota
+function notasFinales(){
+    for (i = 1 ; i <= alumnos ; i++) {
+        nota = parseInt(prompt("Ingrese la nota final del alumno"));
+        notaAcum = notaAcum + nota
+    }
 }
 
+notasFinales();
 
 function promedio () {
     prom = notaAcum / alumnos
-    alert ("El promedio de nota de los alumnos de la clase es: " + prom);
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = "El promedio de nota de los alumnos de la clase es: " + prom
+    document.body.appendChild(contenedor);
 }
 
 promedio ();
 
 if (prom < 4) {
-    alert ("La mayoría de los alumnos fueron reprobados. Deberíamos revisar el plan de estudios o los métodos de enseñanza");
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = "La mayoría de los alumnos fueron reprobados. Deberíamos revisar el plan de estudios o los métodos de enseñanza";
+    document.body.appendChild(contenedor);
 } else if (prom > 7) {
-    alert ("Felicitaciones, la mayoría de los alumnos promocionaron.")
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = "Felicitaciones, la mayoría de los alumnos promocionaron.";
+    document.body.appendChild(contenedor);
 } else {
-    alert ("La mayoría de los alumnos alumnos son regulares para la cursada.")
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = "La mayoría de los alumnos alumnos son regulares para la cursada.";
+    document.body.appendChild(contenedor);
 }
+
+
+
+
+
 
 class Alumno {
     constructor (nombre, apellido, notaP1, notaP2) {
@@ -44,7 +59,7 @@ class Alumno {
     }
 
     aprobacionAlumno (nombre, apellido, notaP1, notaP2) {
-        if ( ((notaP1 + notaP2) / 2 > 7)  && (notaP1 && notaP2 >= 4) ) {
+        if ( ((notaP1 + notaP2) / 2 >= 7)  && (notaP1 && notaP2 >= 4) ) {
             return "El alumno " + nombre + " " + apellido + " está promocionado";
         } 
         else if 
@@ -63,25 +78,30 @@ class Alumno {
 
 let cursoAlumnos = []
 
-for (i=0; i<alumnos; i++) {
-let nombre = prompt("Ingrese el nombre del alumno");
-let apellido = prompt("Ingrese el apellido del alumno");
-let notaP1 = parseInt(prompt("Ingrese la nota del primer parcial"));
-let notaP2 = parseInt(prompt("Ingrese la nota del segundo parcial"));
-
-let alumno1 = new Alumno(nombre, apellido, notaP1, notaP2);
-cursoAlumnos.push(alumno1);
-
-let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<h3> Nombre: ${alumno1.nombre} ${alumno1.apellido} </h3>
-                            <ul> 
-                                <li> Nota primer parcial: ${alumno1.notaP1} </li>
-                                <li> Nota segundo parcial: ${alumno1.notaP2} </li>
-                                <li> ${alumno1.aprobacionAlumno(nombre, apellido, notaP1, notaP2)} </li>
-                            </ul>`;
-    document.body.appendChild(contenedor);
-
+function ingresarDatosAlumnos(){
+    
+    for (i=0; i<alumnos; i++) {
+    let nombre = prompt("Ingrese el nombre del alumno");
+    let apellido = prompt("Ingrese el apellido del alumno");
+    let notaP1 = parseInt(prompt("Ingrese la nota del primer parcial"));
+    let notaP2 = parseInt(prompt("Ingrese la nota del segundo parcial"));
+    
+    let alumno1 = new Alumno(nombre, apellido, notaP1, notaP2);
+    cursoAlumnos.push(alumno1);
+    
+    let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h4 class="reset"> Alumno: ${alumno1.nombre} ${alumno1.apellido} </h4>
+                                <ul class="reset"> 
+                                    <li> Nota primer parcial: ${alumno1.notaP1} </li>
+                                    <li> Nota segundo parcial: ${alumno1.notaP2} </li>
+                                    <li> ${alumno1.aprobacionAlumno(nombre, apellido, notaP1, notaP2)} </li>
+                                </ul>`;
+        document.body.appendChild(contenedor);
+    
+    }
 }
+
+ingresarDatosAlumnos();
 
 //console.log(alumno1.nombre);
 //console.log(alumno1.promedio(notaP1, notaP2));
@@ -106,4 +126,17 @@ console.log(buscarAlumno);
 
 const promocionados = cursoAlumnos.filter ((alumno) => alumno.notaP1 >= 7 && alumno.notaP2 >=7); // Método de filtrado: crea un nuevo array con los alumnos promocionados
 console.log(promocionados);
+
+
+function add() {
+    alumnos = Number(prompt("Ingrese la cantidad de alumnos que quiere agregar."));
+    ingresarDatosAlumnos();
+}
+
+// Problema: esto aparece automáticamente en vez de onclick. 
+let buttonAdd = document.getElementById("btn-add")
+buttonAdd.addEventListener("click", add)
+
+
+
 
